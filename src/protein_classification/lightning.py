f68cbcfebb62aaf8759e6d534a3ecdb50bda11e0
federico-carrara <federico1.carrara@mail.polimi.it>
2025-07-04 10:38:56 +0200

    feat: added computation of F1 score in lightning

--- src/protein_classification/lightning.py
+++ src/protein_classification/lightning.py
@@ -1,8 +1,7 @@
 import pytorch_lightning as pl
 import torch
 
-import torch.nn as nn
-import torch.nn.functional as F
+from torchmetrics.classification import MulticlassF1Score
 
 from protein_classification.config import AlgorithmConfig
 from protein_classification.config.losses import loss_factory
@@ -14,7 +13,14 @@ class BioStructClassifier(pl.LightningModule):
         super().__init__()
         self.config = config
         self.model = DenseNet(**config.architecture_config.model_dump())
-        self.loss_fn = loss_factory(config.loss_config)        
+        self.loss_fn = loss_factory(config.loss_config)
+        
+        # metrics
+        self.f1_metric = MulticlassF1Score(
+            num_classes=config.architecture_config.num_classes,
+            average='macro',
+            compute_on_step=False
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
@@ -26,13 +32,17 @@ class BioStructClassifier(pl.LightningModule):
         logits: torch.Tensor = self.model(x)
         loss = self.loss_fn(logits, y)
         acc = (logits.argmax(dim=1) == y).float().mean()
-        # TODO: add precision, recall, F1
+        f1 = self.f1_metric(logits, y)
         self.log(
             'train_loss', loss, prog_bar=True,
             on_step=True, on_batch=True, batch_size=x.size(0)
         )
         self.log(
-            'train_acc', acc, prog_bar=True,
+            'train_accuracy', acc, prog_bar=True,
+            on_step=True, on_batch=True, batch_size=x.size(0)
+        )
+        self.log(
+            'train_f1', f1, prog_bar=True,
             on_step=True, on_batch=True, batch_size=x.size(0)
         )
         return loss
@@ -44,13 +54,17 @@ class BioStructClassifier(pl.LightningModule):
         logits: torch.Tensor = self(x)
         loss = self.loss_fn(logits, y)
         acc = (logits.argmax(dim=1) == y).float().mean()
-        # TODO: add precision, recall, F1
+        f1 = self.f1_metric(logits, y)
         self.log(
             'val_loss', loss, prog_bar=True,
             on_step=True, on_batch=True, batch_size=x.size(0)
         )
         self.log(
-            'val_acc', acc, prog_bar=True,
+            'val_accuracy', acc, prog_bar=True,
+            on_step=True, on_batch=True, batch_size=x.size(0)
+        )
+        self.log(
+            'val_f1', f1, prog_bar=True,
             on_step=True, on_batch=True, batch_size=x.size(0)
         )
 
