c14eff1384a5fd65ab307ad92239f4d9c567a0ab
federico-carrara <federico1.carrara@mail.polimi.it>
2026-03-25 13:11:25 +0000

    added subdirs and modules for resnet implementation

--- src/protein_classification/config/__init__.py
+++ src/protein_classification/config/__init__.py
@@ -1,5 +1,4 @@
 from .algorithm import AlgorithmConfig
-from .architecture import DenseNetConfig
 from .data import DataConfig, DataAugmentationConfig
 from .losses import LossConfig
 from .training import TrainingConfig
